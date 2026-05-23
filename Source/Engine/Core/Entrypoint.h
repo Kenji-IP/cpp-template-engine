400f4ddafaf971a409ce8ad3e800bb70e951905f
Kenji Ilham Pratama <>
2026-05-24 05:01:39 +0700

    Initial Commit

---
+++ Source/Engine/Core/Entrypoint.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Engine/Utils/Types.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
---
+++ Source/Engine/Utils/Types.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+using u8    = std::uint8_t;
+using u16   = std::uint16_t;
+using u32   = std::uint32_t;
+using u64   = std::uint64_t;
+
+using i8    = std::int8_t;
+using i16   = std::int16_t;
+using i32   = std::int32_t;
+using i64   = std::int64_t;
+
+using f32   = float;
+using f64   = double;
+
+using usize = std::size_t;
+using isize = std::ptrdiff_t;
+
+struct Ignore
+{
+    template <typename T>
+    constexpr const Ignore& operator=(const T&) const noexcept { return *this; } 
+};
+
+inline const Ignore yeet = {};
+
+using Handle = std::uintptr_t;
